0411a003558ea0fc753471dc203afc495aaf6d42
svshu <shumilin845@gmail.com>
2019-03-06 21:13:14 +0300

    Передача списка объектов на jsp

--- src/main/java/com/shop/sport/MainServlet.java
+++ src/main/java/com/shop/sport/MainServlet.java
@@ -8,14 +8,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet("/")
 public class MainServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        Product product = new Product(1, "Nike", "sniekers", 95);
-        req.setAttribute("product", product);
+        Product nike = new Product(1, "Nike", "sneakers", 95);
+        Product adidas = new Product(2, "Adidas", "sneakers", 100);
+        Product puma = new Product(3, "Puma", "sneakers", 85);
+
+        List<Product> products = new ArrayList<>();
+        products.add(nike);
+        products.add(adidas);
+        products.add(puma);
+        req.setAttribute("products", products);
 
         req.getRequestDispatcher("main.jsp").forward(req, resp);
     }
--- src/main/webapp/main.jsp
+++ src/main/webapp/main.jsp
@@ -1,9 +1,12 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri = "http://java.sun.com/jsp/jstl/core" prefix = "c" %>
 <html>
 <head>
     <title>Sport shop</title>
 </head>
 <body>
-Product: ${product.type} name: ${product.name} price: $${product.price}
+<c:forEach var="product" items="${products}">
+    <p>Product: ${product.type} name: ${product.name} price: ${product.price}</p>
+</c:forEach>
 </body>
 </html>
